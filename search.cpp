1c1cc3f278f1b1b98423887cb9bd72ce405c6ce9
Ashwin Shenoy <aswinshenoy65@gmail.com>
2017-12-11 17:45:14 +0530

    Updated to v.1.3
    *Improved search algorithm, fixed bugs
    *Organised search process into functions

--- search.cpp
+++ search.cpp
@@ -1,23 +1,71 @@
+/*
+	FILENAME : SEARCH.CPP
+	DESCRIPTION: File holding the search feature
+*/
+
+/* FUNCTION TO DISPLAY RESULTS */
+int show_results(flight rs[50][2], int rf[50], int n)
+{
+	int x=6, y=5;
+	txy("#",x,y);
+	txy("FLIGHT NO.",x+5,y);
+	txy("DEPART.",x+25,y);
+	txy("ARRIVAL",x+35,y);
+	txy("DUR.",x+45,y);
+	txy("STOPS",x+55,y);
+	txy("FARE",x+65,y++);
+	prntln(y++);
+
+	for(int i=0; i<n; i++)
+	{
+		gotoxy(x,y); cout<<i+1;
+		//Direct Flights
+		if(rf[i]==0)
+		{
+			txy(rs[i][0].airline,x+5,y); cout<<rs[i][0].code;
+			gotoxy(x+25,y); prnttime(rs[i][0].departure);
+			gotoxy(x+35,y); prnttime(rs[i][0].arrival);
+			gotoxy(x+45,y); cout<<timedif(rs[i][0].departure,rs[i][0].arrival)<<"hrs";
+			gotoxy(x+55,y); cout<<"0";
+			txy("Rs. ",x+65,y); cout<<rs[i][0].getfare();
+
+		}
+		//Connection Flights
+		else if(rf[i]==1)
+		{
+			gotoxy(x+5,y);
+			cout<<rs[i][0].airline<<rs[i][0].code<<", "<<rs[i][1].airline<<rs[i][1].code;
+			gotoxy(x+25,y); prnttime(rs[i][0].departure);
+			gotoxy(x+35,y); prnttime(rs[i][1].arrival);
+			gotoxy(x+45,y);
+			cout<<timedif(rs[i][0].departure,rs[i][1].arrival)<<"hrs";
+			gotoxy(x+55,y); cout<<"1 ("<<rs[i][0].dest<<")";
+			txy("Rs. ",x+65,y); cout<<rs[i][0].getfare()+rs[i][1].getfare();
+		}
+		delay(150); y++;
+	}
+	txy("USE ARROW KEYS TO SELECT FLIGHT, AND ENTER KEY TO VIEW DETAILS", maxx/2,maxy-2,0,1);
+	int op = selopy(2,7,--y); op--;
+	return op;
+}
+
+/* FUNCTION HOLDING THE SEARCH FEATURE OF QUIGO */
 void flight_search()
 {
-	clrscr();
-	
 	/* USER QUERY IS ACCEPTED */
-	
 	char source[5], dest[5];
-	txy("QUIGO SEARCH",maxx/2,2,0,1);
-	txy("BOOK TICKET FASTER & EASIER", maxx/2,3,20,1);
-	txy("Source: ",maxx/2,8,0,2);
-	txy("Destination: ",maxx/2,9,0,2);
-	txy("Sort By: ", maxx/2,10,0,2);
-	char *types[] = {"CHEAPEST","EARLIEST","FASTEST"};
-	gotoxy(maxx/2,8); cin>>source;
-	gotoxy(maxx/2,9); cin>>dest;
-	int type = selopx(types,maxx/2,maxx/2+21,10,3);
+	clrscr();
+	quigo_logo(maxx/2-22,8);
+	txy("ADVANCED FLIGHT SEARCH",maxx/2,10,0,1);
+	txy("ENTER JOURNEY DETAILS",maxx/2,14,0,1);
+	txy("Source: ",maxx/2,15,0,2);
+	txy("Destination: ",maxx/2,16,0,2);
+	txy("ENTER IATA CODE OF AIRPORT, LIKE COK FOR KOCHI.",maxx/2,maxy-2,0,1);
+	gotoxy(maxx/2,15); cin>>source;
+	gotoxy(maxx/2,16); cin>>dest;
 
-	
-	/* PRIMARY SEARCH  FOR DIRECT FLIGHT MATCHES*/
-	flight f, df[25], options[50];
+	/* PRIMARY SEARCH LOGIC FOR DIRECT FLIGHT MATCHES*/
+	flight f, df[30], options[50];
 	int d=0, o=0, c=0;
 	ifstream fin;
 	fin.open("flights.bin",ios::in|ios::binary);
@@ -27,29 +75,32 @@ void flight_search()
 		{
 			if(strcmpi(f.dest,dest)==0)
 			{ df[d] = f; d++; }
-			else if(o<50)  //ALL FLIGHTS FROM SOURCE IS TRACKED
+			else if(o<50)  //FLIGHTS FROM SOURCE IS TRACKED
 			{ options[o] = f; o++; }
 		}
 	}
 	fin.close();
-	
-	/* RESULT IS ALWAYS SORTED FOR LEAST FARE, REGARDGLESS OF USER-SORT REQUEST */
-	sortflights_byfare(df,d);
+
+	/* LEAST FARE OPIONS ARE PREFERRED FIRST FOR CONNECTION FLIGHTS */
 	sortflights_byfare(options,o);
-	
-	/* SECONDARY SEARCH  FOR 1-STOP FLIGHT MATCHES*/
-	flight cf[2][50];
+
+	/* SECONDARY SEARCH LOGIC FOR 1-STOP FLIGHT MATCHES*/
+	flight cf[25][2];
 	fin.open("flights.bin",ios::in|ios::binary);
-	while(fin.read((char*)&f,sizeof(f)))
+	while(fin.read((char*)&f,sizeof(f))&&c<25)
 	{
+		//loops across all options
 		for(int k=0; k<o; k++)
 		{
+			//Is the flight from where option flight reaches 
 			if(strcmpi(f.source,options[k].dest)==0)
 			{
+				//Does the flight goes to search destination
 				if(strcmpi(f.dest,dest)==0)
 				{
-				    //Checks if time-gap b/w connection flights
-					if(f.departure>options[k].arrival+100) //1 hrs is set as default
+					//Is there sufficient time between connection flights?
+					int gap = f.departure-options[k].arrival;
+					if((gap>100)&&(gap<800))  //100 = 1 hour
 				    {
 						cf[c][0] = options[k];
 						cf[c][1] = f;
@@ -60,187 +111,131 @@ void flight_search()
 		}
 	}
 	fin.close();
-	
+
 	/* CHECKS IF QUERY RETURNS ANY RESULT */
-	
+	char type;
 	if(d!=0||c!=0)
 	{
-		int i=0, j=0, n=0, x=6, y=5;
-		clrscr();
-		flight rs[2][25]; int rf[25];
-		
-		cout<<"SHOWING FLIGHTS FROM "<<source<<" TO "<<dest<<endl;
-		cout<<"SORTED BY "<<types[type];
-		
-		txy("#",x,y);
-		txy("FLIGHT NO.",x+5,y);
-		txy("DEPART.",x+25,y);
-		txy("ARRIVAL",x+35,y);
-		txy("DUR.",x+45,y);
-		txy("STOPS",x+55,y);
-		txy("FARE",x+65,y++);
-		prntln(y++);
-
-		/* RESULTS FROM PRIMARY & SECONDARY SEARCHES ARE MERGED AND SORTED */
-		
-		/* RESULTS SORTED BASED ON FARE */
-
-		if(type==0)
-		{   
-			for(i=0, j=0; (i<d||j<c)&&n<15;)
-			{
-				float cfare= cf[j][0].getfare()+cf[j][1].getfare();
-				if(i==d||cfare<df[i].getfare()&&j!=c)
-				{
-					n++; 
-					gotoxy(x+5,y);
-					cout<<cf[j][0].airline<<cf[j][0].code<<", "<<cf[j][1].airline<<cf[j][1].code;
-					gotoxy(x+25,y); prnttime(cf[j][0].departure);
-					gotoxy(x+35,y); prnttime(cf[j][1].arrival);
-					gotoxy(x+45,y);
-					cout<<timedif(cf[j][0].departure,cf[j][1].arrival)<<"hrs";
-					gotoxy(x+55,y); cout<<"1 ("<<cf[j][0].dest<<")";
-					txy("Rs. ",x+65,y); cout<<cfare;
-					delay(150); y++;
-					rs[n][0]=cf[j][0];
-					rs[n][1]=cf[j][1];
-					rf[n]=1; j++;
-				}
-				if((j==c||cfare>df[i].getfare())&&i!=d)
-				{
-					n++;
-					txy(df[0].airline,x+5,y); cout<<df[0].code;
-					gotoxy(x+25,y); prnttime(df[0].departure);
-					gotoxy(x+35,y); prnttime(df[0].arrival);
-					gotoxy(x+45,y); cout<<timedif(df[0].departure,df[0].arrival)<<"hrs";
-					gotoxy(x+55,y); cout<<"0";
-					txy("Rs. ",x+65,y); cout<<df[0].getfare();
-					delay(150); y++;
-					rs[n][0]=df[i]; rf[n]=0;
-					 i++;
-				}
-			}
-		}
-		
-		/* RESULTS SORTED BASED ON EARLIEST ARRIVAL*/
-		
-		if(type==1)
-		{
-			sortflights_byarrival(df,d);
+		char sop;
+		do{
+			int i=0, j=0, n=0;
+			flight rs[50][2]; //holds the results
+			int rf[50]; //holds whether the result is direct(0)/connection(1) type 
 			
-			flight temp[2];
-			for(i=0; i<c; i++)
+			//User is asked for sort-option
+			clrscr();
+			cout<<"SHOWING FLIGHTS FROM "<<source<<" TO "<<dest<<endl;
+			cout<<"SORTED BY ";
+			char *types[] = {"CHEAPEST","EARLIEST","FASTEST"};
+			type = selopx(types,12,30,2,3);
+			
+			/* DIRECT FLIGHT AND CONNECTION FLIGHT RESULTS ARE MERGED AND SORTED */
+
+			/* RESULTS ARE INITIALLY SORTED BASED ON FARE */
+			sortflights_byfare(df,d);
+			sortflights_byfare(cf,c);
+			
+			/* RESULTS SORTED BY CHEAPEST FARE */
+			if(type==0)
 			{
-				for(j=0; j<(c-1)-i; j++)
-					if(cf[j][1].arrival>cf[j+1][1].arrival)
+				//Arrays of Direct and Connection Flights are already sorted
+				//Merging the results
+				for(i=0, j=0; i<d||j<c;)
+				{
+					float cfare= cf[j][0].getfare()+cf[j][1].getfare();
+					
+					//Connected Flight
+					if(i==d||cfare<df[i].getfare()&&j!=c)
 					{
-						temp[0]=cf[j][0]; temp[1]=cf[j][1];
-						cf[j][0]=cf[0][j+1]; cf[1][j]=cf[1][j+1];
-						cf[j+1][0]=temp[0]; cf[j+1][1]=temp[1];
+						rs[n][0]=cf[j][0];
+						rs[n][1]=cf[j][1];
+						rf[n]=1; n++; j++;
 					}
-			}
-			
-			
-			for(i=0,j=0; (i<d||j<c)&&n<15;)
-			{
-				if((i==d||(cf[j][1].arrival<df[i].arrival))&&j!=c)
-				{
-					n++;
-					gotoxy(x+5,y);
-					cout<<cf[j][0].airline<<cf[j][0].code<<", "<<cf[j][1].airline<<cf[j][1].code;
-					gotoxy(x+25,y); prnttime(cf[j][0].departure);
-					gotoxy(x+35,y); prnttime(cf[j][1].arrival);
-					gotoxy(x+45,y);
-					cout<<timedif(cf[j][0].departure,cf[j][1].arrival)<<"hrs";
-					gotoxy(x+55,y); cout<<"1 ("<<cf[j][0].dest<<")";
-					txy("Rs. ",x+65,y); cout<<cf[j][0].getfare()+cf[j][1].getfare();
-					delay(150); y++;
-					rs[n][0]=cf[j][0];
-					rs[n][1]=cf[j][1];
-					rf[n]=1; j++;
-				}
-				if((j==c||(cf[j][1].arrival>df[i].arrival))&&i!=d)
-				{
-					n++;
-					txy(df[0].airline,x+5,y); cout<<df[0].code;
-					gotoxy(x+25,y); prnttime(df[0].departure);
-					gotoxy(x+35,y); prnttime(df[0].arrival);
-					gotoxy(x+45,y); cout<<timedif(df[0].departure,df[0].arrival)<<"hrs";
-					gotoxy(x+55,y); cout<<"0";
-					txy("Rs. ",x+65,y); cout<<df[0].getfare();
-					delay(150); y++;
-					rs[n][0]=df[i]; rf[n]=0;
-					i++;
-				}
-			}
-		}
-
-		/* RESULTS SORTED BASED ON SHORTEST DURATION */
-
-		if(type==2)
-		{
-			sortflights_byduration(df,d);
-
-			for(i=0; i<d; i++)
-			{
-					n++;
-					txy(df[0].airline,x+5,y); cout<<df[0].code;
-					gotoxy(x+25,y); prnttime(df[0].departure);
-					gotoxy(x+35,y); prnttime(df[0].arrival);
-					gotoxy(x+45,y); cout<<timedif(df[0].departure,df[0].arrival)<<"hrs";
-					gotoxy(x+55,y); cout<<"0";
-					txy("Rs. ",x+65,y); cout<<df[0].getfare();
-					delay(150); y++;
-					rs[n][0]=df[i]; rf[n]=0;
-			}
-
-			flight temp[2];
-			for(i=0; i<c; i++)
-			{
-				for(j=0; j<(c-1)-i; j++)
-					if(abs(cf[j][1].arrival-cf[j][0].departure)>abs(cf[j+1][1].arrival-cf[j+1][0].departure))
+					
+					//Direct Flight
+					if((j==c||cfare>df[i].getfare())&&i!=d)
 					{
-						temp[0]=cf[0][j]; temp[1]=cf[j][1];
-						cf[j][0]=cf[0][j+1]; cf[j][1]=cf[j+1][1];
-						cf[j+1][0]=temp[0]; cf[j+1][1]=temp[1];
+
+						rs[n][0]=df[i]; 
+						rf[n]=0; n++; i++;
 					}
+				}
 			}
 
-			for(j=0; j<c; j++)
+			/* RESULTS SORTED BASED ON EARLIEST ARRIVAL*/
+			if(type==1)
 			{
-					n++;
-					gotoxy(x+5,y);
-					cout<<cf[j][0].airline<<cf[j][0].code<<", "<<cf[j][1].airline<<cf[j][1].code;
-					gotoxy(x+25,y); prnttime(cf[j][0].departure);
-					gotoxy(x+35,y); prnttime(cf[j][1].arrival);
-					gotoxy(x+45,y);
-					cout<<timedif(cf[j][0].departure,cf[j][1].arrival)<<"hrs";
-					gotoxy(x+55,y); cout<<"1 ("<<cf[j][0].dest<<")";
-					txy("Rs. ",x+65,y); cout<<cf[j][0].getfare()+cf[j][1].getfare();
-					delay(150); y++;
-					rs[n][0]=cf[j][0];
-					rs[n][1]=cf[j][1];
-					rf[n]=1;
-			}
-		}
+				//Sorting the results
+				sortflights_byarrival(df,d);
+				sortflights_byarrival(cf,c);
+				
+				//Merging the results
+				for(i=0, j=0; i<d||j<c;)
+				{
+					//Connected Flight
+					if((i==d||(cf[j][1].arrival<df[i].arrival))&&j!=c)
+					{
+						rs[n][0]=cf[j][0];
+						rs[n][1]=cf[j][1];
+						rf[n]=1; n++; j++;
+					}
 
-		txy("USE ARROW KEYS TO SELECT FLIGHT, AND ENTER KEY TO VIEW DETAILS", maxx/2,maxy-2,0,1);
-		int op = selopy(2,7,--y);
-		
-		/* INDIVIDUAL RESULTS DISPLAYED BASED ON SELECTION*/
-		clrscr();
-		float totalfare;
-		flight rd = rs[op][0];
-		rd.show_flightdetails(4,maxy/2);
-		totalfare += f.getfare();
-		if(rf[op]==1)
-		{
-			rs[op][1].show_flightdetails(maxx/2,maxy/2);
-			totalfare += f.getfare();
-			txy("TOTAL FARE: Rs.", maxx/2,maxy/2+8,0,1); cout<<totalfare;
-		}
-		getch();
+					//Direct Flight
+					else if(i!=d)
+					{
+						rs[n][0]=df[i]; 
+						rf[n]=0; n++; i++;
+					}
+				}
+			}
+
+			/* RESULTS SORTED BASED ON SHORTEST DURATION */
+			if(type==2)
+			{
+				//Sorting the results
+				sortflights_byduration(df,d);
+				sortflights_byduration(cf,c);
+
+				//Direct Flights always take shorter duration
+				for(i=0; i<d; i++)
+				{
+						rs[n][0]=df[i];
+						rf[n]=0; n++;
+				}
+				
+				//Connection Flights take more duration
+				for(j=0; j<c; j++)
+				{
+						rs[n][0]=cf[j][0];
+						rs[n][1]=cf[j][1];
+						rf[n]=1; n++;
+				}
+			}
+			//Selector allowing to view flight details
+			int op = show_results(rs,rf,n);
+			
+			
+			/* INDIVIDUAL RESULTS DISPLAYED BASED ON SELECTION*/
+			clrscr();
+			txy("VIEWING FLIGHT DETAILS",4,2);
+			flight rd = rs[op][0];
+			rd.show_flightdetails(4,maxy/2);
+			if(rf[op]==1)
+			{
+				rs[op][1].show_flightdetails(maxx/2+4,maxy/2);
+				txy("FLIGHT #1",4,4); txy("FLIGHT #2",maxx/2+4,4);
+				txy("FARE: Rs.", 4,maxy-8); 
+				cout<<rs[op][0].getfare()+rs[op][1].getfare();
+				txy("LAYOVER TIME: ",4,maxy-7); 
+				cout<<timedif(rs[op][0].arrival,rs[op][1].departure)<<"hrs";
+				txy("DURATION: ",4,maxy-6); 
+				cout<<timedif(rs[op][0].departure,rs[op][1].arrival)<<"hrs";
+			}
+			txy("PRESS ESC KEY TO EXIT, OR ANY OTHER KEY TO RETURN TO RESULTS.",maxx/2,maxy-2,0,1);
+			sop = getch();
+
+		}while((int)sop!=27);
 	}
 	else
-	{ clrscr(); txy("NO FLIGHTS FOUND!",maxx/2,maxy/2,0,1); getch(); }
+	{ clrscr(); txy("NO FLIGHTS WERE FOUND!",maxx/2,maxy/2,0,1); getch(); }
 }
